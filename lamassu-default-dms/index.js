be7b684e5b815fac4a1d6c6afc516d968d71720b
hsaiz <hsaiz@ikerlan.es>
2021-07-30 10:14:21 +0000

    New steps and env vars added

--- lamassu-default-dms/index.js
+++ lamassu-default-dms/index.js
@@ -3,20 +3,22 @@ const util = require("util");
 const fs = require('fs');
 const exec = util.promisify(require("child_process").exec);
 
-const DOMAIN = process.env.DOMAIN
-
-
 const options = {
   key: fs.readFileSync('/app/https.key'),
   cert: fs.readFileSync('/app/https.crt')
 };
 
 const execCmd = async (cmd) => {
-  exec_res = await exec(cmd);
-  if (exec_res.error == null) {
-    return 0 
-  } else {
-    console.log(exec_res.error);
+  try{
+    exec_res = await exec(cmd);
+    if (exec_res.error == null) {
+      return 0 
+    } else {
+      console.log(exec_res.error);
+      return -1
+    }
+  } catch (er){
+    console.log(er);
     return -1
   }
 }
@@ -27,7 +29,18 @@ const server = https.createServer(options, async (req, res) => {
   
   
   if (req.url.startsWith("/dms-renew/") && req.method === "POST") {
-    const deviceId = req.url.split("/dms-issue/")[1]
+    console.log(req.url);
+    const deviceId = req.url.split("/dms-renew/")[1]
+    console.log(deviceId);
+    const aps = "Lamassu-Root-CA2-RSA2048"
+    const CMD_ENROLL = 'estclient reenroll -server lamassu.zpd.ikerlan.es:9998 -explicit /app/device-manager-anchor.crt -csr /app/devices-crypto-material/device-'+deviceId+'.csr -out /app/devices-crypto-material/device-reenrolled-'+deviceId+'.crt -aps ' + aps + ' -certs /app/devices-crypto-material/device-'+deviceId+'.crt -key /app/devices-crypto-material/device-'+deviceId+'.key' ;
+    if (await execCmd(CMD_ENROLL) == 0) {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Executed correctly" }));
+    } else {
+      res.writeHead(500, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Internal server error" }));
+    }
 
   } else if (req.url.startsWith("/dms-issue/csr/") && req.method === "POST") {
     const cn_aps = req.url.split("/dms-issue/csr/")[1]
@@ -40,7 +53,7 @@ const server = https.createServer(options, async (req, res) => {
       const data = Buffer.concat(chunks).toString("utf-8");
 
       fs.writeFileSync('/app/devices-crypto-material/device-'+cn+'.csr', data)
-      const CMD_ENROLL = 'estclient enroll -server '+DOMAIN+':9998 -explicit /app/device-manager-anchore.crt -csr /app/devices-crypto-material/device-'+cn+'.csr -out /app/devices-crypto-material/device-'+cn+'.crt -aps ' + aps + ' -certs /app/enrolled-dms.crt -key /app/enrolled-dms.key' ;
+      const CMD_ENROLL = 'estclient enroll -server lamassu.zpd.ikerlan.es:9998 -explicit /app/device-manager-anchor.crt -csr /app/devices-crypto-material/device-'+cn+'.csr -out /app/devices-crypto-material/device-'+cn+'.crt -aps ' + aps + ' -certs /app/enrolled-dms.crt -key /app/enrolled-dms.key' ;
       console.log(CMD_ENROLL);
       if (await execCmd(CMD_ENROLL) == 0) {
         res.writeHead(200, { "Content-Type": "application/json" });
@@ -71,7 +84,8 @@ const server = https.createServer(options, async (req, res) => {
       if (data.key_type == "ec") {
         CMD_GEN_CSR = 'openssl req -nodes -newkey ec -pkeyopt ec_paramgen_curve:secp' + data.key_bits +'r1  -keyout /app/devices-crypto-material/device-'+cn+'.key -out /app/devices-crypto-material/device-'+cn+'.csr -subj "/C=' + c +'/ST=' + st +'/L=' + l +'/O=' + o +'/OU=' + ou +'/CN='+cn+'"'
       }else{
-        CMD_GEN_CSR = 'openssl req -nodes -newkey rsa:' + data.key_bits +' -keyout /app/devices-crypto-material/device-'+cn+'.key -out /app/devices-crypto-material/device-'+cn+'.csr -subj "/C=' + c +'/ST=' + st +'/L=' + l +'/O=' + o +'/OU=' + ou +'/CN='+cn+'"'
+        // CMD_GEN_CSR = 'openssl req -nodes -newkey rsa:' + data.key_bits +' -keyout /app/devices-crypto-material/device-'+cn+'.key -out /app/devices-crypto-material/device-'+cn+'.csr -subj "/C=' + c +'/ST=' + st +'/L=' + l +'/O=' + o +'/OU=' + ou +'/CN='+cn+'"'
+        CMD_GEN_CSR = 'openssl req -nodes -newkey rsa:' + data.key_bits +' -keyout /app/devices-crypto-material/device-'+cn+'.key -out /app/devices-crypto-material/device-'+cn+'.csr -subj "/CN='+cn+'"'
       }
 
       console.log(CMD_GEN_CSR);
@@ -80,7 +94,7 @@ const server = https.createServer(options, async (req, res) => {
         res.writeHead(500, { "Content-Type": "application/json" });
         res.end(JSON.stringify({ message: "Executed correctly" }));
       }else{
-        const CMD_ENROLL = 'estclient enroll -server '+DOMAIN+':9998 -explicit /app/device-manager-anchore.crt -csr /app/devices-crypto-material/device-'+cn+'.csr -out /app/devices-crypto-material/device-'+cn+'.crt -aps ' + aps + ' -certs /app/enrolled-dms.crt -key /app/enrolled-dms.key' ;
+        const CMD_ENROLL = 'estclient enroll -server lamassu.zpd.ikerlan.es:9998 -explicit /app/device-manager-anchor.crt -csr /app/devices-crypto-material/device-'+cn+'.csr -out /app/devices-crypto-material/device-'+cn+'.crt -aps ' + aps + ' -certs /app/enrolled-dms.crt -key /app/enrolled-dms.key' ;
         console.log(CMD_ENROLL);
         if (await execCmd(CMD_ENROLL) == 0) {
           res.writeHead(200, { "Content-Type": "application/json" });

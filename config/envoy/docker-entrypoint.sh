9f51e05ec4c2364cd9220ed300f7550b9fc85f78
HaritzSaiz <hsaiz@ikerlan.es>
2022-02-04 15:54:51 +0000

    Fixed envoy proxy docker-entrypoint

--- config/envoy/docker-entrypoint.sh
+++ config/envoy/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 echo "Generating envoy.yaml config file..."
 cat /tmpl/envoy.yaml.tmpl | envsubst \$DOMAIN > /etc/envoy.yaml
 
-if [ $DEBUG_MODE == true ]
+if [ $DEBUG_MODE == "true" ]
 then
     echo "Starting Envoy in debug mode..."
     /usr/local/bin/envoy -c /etc/envoy.yaml -l debug
